4e1e053d5c760eab9fad2cae7e421dba82cbbb44
Diego Sánchez <diego@sanch.dev>
2022-12-30 00:08:57 -0300

    Documentation

--- Day-7-No-Space-Left/src/inode.rs
+++ Day-7-No-Space-Left/src/inode.rs
@@ -1,5 +1,11 @@
+/// File system inode
 pub trait INode {
+    /// Returns the `size` of the inode in **bytes**
     fn size(&self) -> u32;
+
+    /// Returns the name of the inode
     fn name(&self) -> &str;
+
+    /// Returns true if the inode is a directory
     fn is_directory(&self) -> bool;
 }
--- Day-7-No-Space-Left/src/parser.rs
+++ Day-7-No-Space-Left/src/parser.rs
@@ -2,6 +2,7 @@ use crate::file_system::FileSystem;
 
 fn parse_file_system(input: &str) -> FileSystem {
     let mut file_sistem = FileSystem::
+
     for line in input.lines() {
 
     }
