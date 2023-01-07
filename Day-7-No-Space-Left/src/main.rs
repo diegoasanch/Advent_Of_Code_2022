9062100e2c1c3b938ddf72d872234df624c4f869
Diego Sánchez <diego@sanch.dev>
2023-01-06 23:10:58 -0300

    Day 7 - part 2

--- Day-7-No-Space-Left/src/main.rs
+++ Day-7-No-Space-Left/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, rc::Rc};
 
 use parser::parse_file_system;
 
@@ -10,6 +10,10 @@ mod file_system;
 mod inode;
 mod parser;
 
+const DISK_SIZE: u32 = 70_000_000;
+const SIZE_LIMIT: u32 = 100_000;
+const REQUIRED_UPDATE_SIZE: u32 = 30_000_000;
+
 fn main() {
     let input = fs::read_to_string("./src/input.txt").expect("Input file not found");
 
@@ -18,14 +22,13 @@ fn main() {
     println!("Tree");
     file_system.print_tree(0);
 
-    let size_limit = 100000u32;
     let all_items = file_system.all_items();
 
     let small_items = all_items
         .iter()
         .filter(|item| {
             let item = item.try_borrow().expect("Dead item reference");
-            item.is_directory() && item.size() < size_limit
+            item.is_directory() && item.size() < SIZE_LIMIT
         })
         .collect::<Vec<&INodeRef>>();
 
@@ -35,6 +38,39 @@ fn main() {
 
     println!(
         "Total size of directories under {} bytes: {}",
-        size_limit, total_size
+        SIZE_LIMIT, total_size
     );
+
+    let total_size = file_system.size();
+    let free_space = DISK_SIZE - total_size;
+
+    let required_delete_size = REQUIRED_UPDATE_SIZE - free_space;
+
+    let smallest_sufficient_directory =
+        find_smallest_sufficient_directory(&all_items, required_delete_size);
+
+    println!(
+        "Smallest directory with sufficient space: {:?}",
+        smallest_sufficient_directory
+    );
+}
+
+fn find_smallest_sufficient_directory(
+    items: &Vec<INodeRef>,
+    required_space: u32,
+) -> Option<INodeRef> {
+    let mut smallest_sufficient_directory = None;
+    let mut smallest_sufficient_directory_size = u32::MAX;
+
+    for item in items {
+        let borrowed_item = item.try_borrow().expect("Dead item reference");
+        if borrowed_item.is_directory() && borrowed_item.size() >= required_space {
+            if borrowed_item.size() < smallest_sufficient_directory_size {
+                smallest_sufficient_directory = Some(Rc::clone(&item));
+                smallest_sufficient_directory_size = borrowed_item.size();
+            }
+        }
+    }
+
+    smallest_sufficient_directory
 }
